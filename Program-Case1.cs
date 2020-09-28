c65dd8a2c1efb9ee8b5d13696ea619483bb00686
SvetlanaL7 <sveta.lyukina1999@mail.ru>
2020-09-28 20:05:13 +0300

    Исходный код для Case1, Case2

---
+++ Program-Case1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            int M = Int32.Parse(Console.ReadLine());
+            int X = Int32.Parse(Console.ReadLine());
+            int Y = Int32.Parse(Console.ReadLine());
+
+            if ((N <= 0) | (M <= 0) | (X < 0) | (Y < 0))
+            {
+                Console.WriteLine("Ошибка! Число не может быть отрицательным!");
+            }
+            else
+            {
+                int dl = 0;
+                int sh = 0;
+                int min = 0;
+                if (N > M) { dl = N; sh = M; } else { dl = M; sh = N; }
+                int X2 = dl - X;
+                int Y2 = sh - Y;
+                if ((X > X2) & (X2 >= 0)) { min = X2; } else { min = X; }
+                if (Y < min) { min = Y; }
+                if ((Y2 < min) & (Y2 >= 0)) { min = Y2; }
+                Console.WriteLine(min);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Program-Case2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int V = Int32.Parse(Console.ReadLine());
+            int T = Int32.Parse(Console.ReadLine());
+
+            int Res = 0;
+
+            int S = Math.Abs(V)*T;
+            if (S >= 109)
+            {
+                int del = (int)(S / 109);
+                int k = del * 109;
+                Res = S - k;
+            }
+            else
+            {
+                Res = S;
+            }
+
+            if (V < 0) { Res = 109 - Res; }
+
+            Console.WriteLine(Res);
+        
+            Console.ReadKey();
+        }
+    }
+}
